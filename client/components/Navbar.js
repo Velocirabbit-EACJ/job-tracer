0824d16b5208cb07344b6202dd19a238fa389398
Colin A. Gibson <45520511+cgefx@users.noreply.github.com>
2021-07-14 23:18:46 -0700

    clean up navbar

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -2,20 +2,14 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { SunIcon, MoonIcon } from '@chakra-ui/icons';
 
-import {
-  Heading,
-  Flex,
-  Button,
-  Link,
-  useColorMode,
-  Stack,
-} from '@chakra-ui/react';
+import { Heading, Flex, Button, Link, useColorMode } from '@chakra-ui/react';
 
 function NavBar() {
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
     <Flex
+      width="100%"
       as="nav"
       align="center"
       justify="space-between"
@@ -24,19 +18,15 @@ function NavBar() {
       shadow="sm"
       borderBottom="1px solid #C8C8C8"
     >
-      {/* app name */}
       <Link as={NavLink} to="/">
         <Heading as="h1" size="lg" mt={[3, 0, 0, 0]} mr={[0, 3, 3, 3]}>
           Job Tracer
         </Heading>
       </Link>
 
-      <Stack direction="row" spacing={4}>
-        {/* dark mode toggler */}
-        <Button rounded="50%" onClick={() => toggleColorMode()}>
-          {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
-        </Button>
-      </Stack>
+      <Button rounded="50%" onClick={() => toggleColorMode()}>
+        {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+      </Button>
     </Flex>
   );
 }
