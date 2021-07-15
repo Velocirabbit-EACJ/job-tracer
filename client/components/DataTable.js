d11f14a1deba9b62bbf83524bcfb697a0ec76a1c
Colin A. Gibson <45520511+cgefx@users.noreply.github.com>
2021-07-14 23:17:38 -0700

    swapping out a stack for a table in the dashboard

--- client/components/DataTable.js
+++ client/components/DataTable.js
@@ -1,8 +1,19 @@
-import { Table, Thead, Tbody, Tr, Th, Td, Box } from '@chakra-ui/react';
-import { FaHeart, FaRegHeart } from 'react-icons/fa';
-import PropTypes from 'prop-types';
-
 import React from 'react';
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  Box,
+  Link,
+  Flex,
+  IconButton,
+} from '@chakra-ui/react';
+import { FaHeart, FaRegHeart, FaEllipsisV, FaTimes } from 'react-icons/fa';
+import PropTypes from 'prop-types';
+import { Link as RouterLink } from 'react-router-dom';
 
 function DataTable({ jobData }) {
   return (
@@ -13,15 +24,35 @@ function DataTable({ jobData }) {
             <Th>Company Name</Th>
             <Th>Status</Th>
             <Th>Favorite</Th>
+            <Th>Actions</Th>
           </Tr>
         </Thead>
         <Tbody>
           {jobData.map((job, i) => (
             <Tr key={`row-${i + 1}`}>
-              <Td>{job.company_name}</Td>
+              <Link as={RouterLink} to={`/jobApplications/${job.id}`}>
+                <Td>{job.company_name}</Td>
+              </Link>
               <Td>{job.status_name}</Td>
-              <Td textAlign="center">
-                {job.favorite ? <FaHeart /> : <FaRegHeart />}
+              <Td>{job.favorite ? <FaHeart /> : <FaRegHeart />}</Td>
+              <Td>
+                <Flex>
+                  <IconButton
+                    variant="outline"
+                    colorScheme="teal"
+                    aria-label="Call Sage"
+                    size="sm"
+                    icon={<FaEllipsisV />}
+                  />
+                  <IconButton
+                    ml={4}
+                    variant="outline"
+                    colorScheme="red"
+                    aria-label="Call Sage"
+                    size="sm"
+                    icon={<FaTimes />}
+                  />
+                </Flex>
               </Td>
             </Tr>
           ))}
